d5751965d9033645598d4b886c35ef98b9f4b6e9
Ethan Warden <ethancwarden@gmail.com>
2024-09-11 08:36:27 -0400

    WIP 1.1

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-let firstCard = 5
-let secondCard = 9
+let firstCard = 10
+let secondCard = 11
+let cards = [firstCard, secondCard]
 let hasBlackJack = false
 let isAlive = true
 let sum = firstCard + secondCard
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    sumEl.textContent = "Sum: " + sum 
+
     if (sum <= 20) {
         console.log("Do you want to draw another card?")
         message = "Do you want to draw another card?"
     }
     else if (sum === 21) {
-        console.loYou've got Blackjack!")
+        console.log("You've got Blackjack!")
         hasBlackJack = true
         message = "You've got Blackjack!"
     }
@@ -22,8 +31,15 @@ function startGame() {
         isAlive = false
         message = "You're out of the game!"
     }
-
-    sumEl.textContent = "Sum: " + sum    
+   
     messageEl.textContent = message
+}
+
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+    let card = 5;
+    sum += card;
+
+    renderGame();
 }
 
